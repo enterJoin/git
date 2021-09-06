190c8c16ea2d0e932f45f8c5a6101ba75e06ee9b
luoxuanwan <1578042115@qq.com>
2021-09-06 17:39:49 +0800

    push commit with idea!!!

--- src/main/java/com/atlxw/git/GitTest.java
+++ src/main/java/com/atlxw/git/GitTest.java
@@ -2,12 +2,11 @@ package com.atlxw.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello Git");
-        System.out.println("Hello Git2");
-        System.out.println("Hello Git3");
         System.out.println("Hello git");
         System.out.println("Hello git2");
         System.out.println("master branch");
         System.out.println("hot-fix branch");
+
+        System.out.println("push 操作成功");
     }
 }
